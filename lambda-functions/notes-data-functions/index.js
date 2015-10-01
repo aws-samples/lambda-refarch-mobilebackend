9aaf470cffdff21353715f592c3a6b292adf5655
waleoladehin <oladehin@amazon.com>
2015-10-01 13:11:42 -0400

    Removed node_modules from notes-data-function

--- lambda-functions/notes-data-functions/index.js
+++ lambda-functions/notes-data-functions/index.js
@@ -1,21 +1,17 @@
-var AWS = require('aws-sdk');
-var DOC = require('dynamodb-doc');
-
-var awsClient = new AWS.DynamoDB();
-var dynamo = new DOC.DynamoDB(awsClient);
+var doc = require('dynamodb-doc');
+var dynamo = new doc.DynamoDB();
 
 exports.handler = function(createPhotoEvent, context ) {
 
     var photoNote = {};
     photoNote.TableName = "PhotoNotes";
-    photoNote.Item = { userId : context.identity.cognitoIdentityId,
-                     photoId : createPhotoEvent.id,
+    photoNote.Item = { photoId : createPhotoEvent.id,
                      headline : createPhotoEvent.headline,
                      s3Url: createPhotoEvent.s3Url };
 
     dynamo.putItem(photoNote, function(err,savedNote){ 
         if(err) {
-            context.fail(new Error('Unable to retrieve user with key: "' + createPhotoEvent.id + '"'));
+            context.fail(new Error('Unable to save photo with key: "' + createPhotoEvent.id + '"'));
         } else {
             context.succeed({success: true});
         }
