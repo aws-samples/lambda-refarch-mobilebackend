22ac8c286d1d524a3ff972a2bde12d01421fd9df
waleoladehin <oladehin@amazon.com>
2015-10-03 09:01:58 -0400

    Updated formatting for stream index function

--- lambda-functions/streams-data-functions/index.js
+++ lambda-functions/streams-data-functions/index.js
@@ -2,40 +2,40 @@ var AWS = require('aws-sdk');
 var cloudSearchDomain = new AWS.CloudSearchDomain({endpoint : 'CLOUDSEARCH_DOCUMENT_ENDPOINT'});
 
 exports.handler = function(event, context) {
-	var records = event.Records;
-	var searchDocuments = createSearchDocuments(records);
+    var records = event.Records;
+    var searchDocuments = createSearchDocuments(records);
 	
     uploadSearchDocuments(context, searchDocuments);
 };
 
 function createSearchDocuments(records) {
-	var searchDocuments = [];
+    var searchDocuments = [];
 	
-	for(var i = 0; i<records.length; i++) {
- 		var record = records[i];
- 		
- 		if (record.eventName === "INSERT") {
-       		var searchDocument = {
-            	type : 'add',
-            	id : record.dynamodb.Keys.noteId.S,
-            	fields : {
-                	headline : record.dynamodb.NewImage.headline.S,
-                	note_text : record.dynamodb.NewImage.text.S
-            	}
-            };
-        	searchDocuments.push(searchDocument);
-    	} 
-	}	
-	return searchDocuments;
+    for(var i = 0; i<records.length; i++) {
+	    var record = records[i];
+		
+	    if (record.eventName === "INSERT") {
+            var searchDocument = {
+                type : 'add',
+                id : record.dynamodb.Keys.noteId.S,
+			    fields : {
+                    headline : record.dynamodb.NewImage.headline.S,
+                    note_text : record.dynamodb.NewImage.text.S
+				}
+			};
+            searchDocuments.push(searchDocument);
+        } 
+    }   
+    return searchDocuments;
 }
 
 function uploadSearchDocuments(context, searchDocuments) {
-	if(searchDocuments.length > 0) {
-		var params = {
+    if(searchDocuments.length > 0) {
+        var params = {
             contentType : 'application/json',
             documents : JSON.stringify(searchDocuments)
         };
-        
+		
         cloudSearchDomain.uploadDocuments(params, function(error, result) {
             if(!error) {
                 context.succeed("Processed " + searchDocuments.length + " search records.");
@@ -45,7 +45,7 @@ function uploadSearchDocuments(context, searchDocuments) {
                 return;
             }
         });
-	} else {
-		context.succeed("No new documents were added to the DynamoDB Table.");
-	}	
+    } else {
+        context.succeed("No new documents were added to the DynamoDB Table.");
+    }   
 }
